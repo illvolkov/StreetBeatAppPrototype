e8e115180216bd68c419f0c49eba92ab58819666
Ilya Volkov <illvolkov@bk.ru>
2022-09-02 18:12:21 +0500

    Initial Commit

---
+++ StreetBeatAppPrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreetBeatAppPrototype
+//
+//  Created by Ilya Volkov on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
